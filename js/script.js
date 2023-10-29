2ff5089e181a847277e6f989a43a07b9397f333c
Jacek Caban <jacekcaban94@gmail.com>
2023-10-29 13:19:54 +0100

    Add change in html and add links loop

--- js/script.js
+++ js/script.js
@@ -34,39 +34,45 @@ function titleClickHandler(event){
     targetArticle.classList.add('active');
 }
 
+const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
 
-    /* remove contents of titleList */
+    /* [done] remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
 
-    /* for each article */
+    /* [done] for each article */
     const articles = document.querySelectorAll(optArticleSelector);
 
     let html = '';
 
     for (let article of articles) {
 
-    /* get the article id */
+    /* [done] get the article id */
     const articleId = article.getAttribute('id');
 
-    /* find the title element */
+    /* [done] find the title element */
 
-    /* get the title from the title element */
+    /* [done] get the title from the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* create HTML of the link */
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     console.log(linkHTML);
 
-    /* insert link into titleList */
+    /* [done] insert link into titleList */
     //titleList.innerHTML = titleList.innerHTML + linkHTML; 
     //titleList.insertAdjacentHTML('beforeend', linkHTML);
 
-    /* insert link into html variable */
+    /* [done] insert link into html variable */
     html = html + linkHTML;
     }
     titleList.innerHTML = html;
