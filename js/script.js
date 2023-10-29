09a29323c130b36270adf13ea6887fdfead266d6
Jacek Caban <jacekcaban94@gmail.com>
2023-10-29 12:21:05 +0100

    Add js code to generating title list

--- js/script.js
+++ js/script.js
@@ -33,12 +33,6 @@ function titleClickHandler(event){
     targetArticle.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
-
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
@@ -50,18 +44,32 @@ function generateTitleLinks(){
 
     /* for each article */
     const articles = document.querySelectorAll(optArticleSelector)
-
-    for (let article of articles)
+    let html = '';
+    for (let article of articles) {
 
     /* get the article id */
+    const articleId = article.getAttribute('id');
 
     /* find the title element */
 
     /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML)
 
     /* insert link into titleList */
+    html = html + linkHTML
+    }
+    console.log(html);
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links)
+
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    }
 
 }
 
