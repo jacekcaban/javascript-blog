766a8831eed4d52fab8a05df4fe44909ea07160c
Jacek Caban <jacekcaban94@gmail.com>
2023-10-29 20:56:25 +0100

    Adding a tag cloud

--- js/script.js
+++ js/script.js
@@ -47,6 +47,7 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
+  optTagsListSelector = '.list .tags',
   optArticleAuthorSelector = '.post-author';
 
 function generateTitleLinks(customSelector = ''){
@@ -93,6 +94,11 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array
+  let allTags = []; */
+  /* [NEW] creat a new variable allTags with an empty object */
+  let allTags = {};
+
   /* [done] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   console.log(articles);
@@ -127,12 +133,37 @@ function generateTags(){
       html = html + linkHTML;
 
     /* [done] END LOOP: for each tag */
+    /* [NEW] check if this link is NOT already in allTags */
+    if(!allTags.hasOwnProperty(tag)){
+      /* [NEW] tag to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
+      console.log(allTags);
     }
     /* [done] insert HTML of all the links into the tags wrapper */
     tagWrapper.innerHTML = html;
     console.log(tagWrapper);
   /* [done] END LOOP: for every article: */
   }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(optTagsListSelector);
+
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP for each tag in allTags: */
+  for(let tag in allTags){
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += tag + ' ('+ allTags[tag] + ') ';
+  }
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTagsHTML;
+  //tagList.innerHTML = allTags.join(' ');
+  console.log(allTags);
 }
 generateTags();
 
