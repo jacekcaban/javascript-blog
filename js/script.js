ffbda738551d11c59e0b10f9cebc887f8805a402
Jacek Caban <jacekcaban94@gmail.com>
2023-10-30 17:45:36 +0100

    Add count for authors

--- js/script.js
+++ js/script.js
@@ -296,7 +296,7 @@ function addClickListenersToTags(){
 }
 addClickListenersToTags();
 
-//[in progress] Add authors to articles
+//[done] Add authors to articles
 function generateAuthors() {
   /* [NEW] create a new variable allAuthors with an empty object */
   let allAuthorsList = {};
